7d776892026baea2eac5f5f7709a66b0252d865b
trevorhutto <huttotw@gmail.com>
2014-05-08 16:32:06 -0400

    Visitor Labeling

--- pages/accounts/more_visitor_info.php
+++ pages/accounts/more_visitor_info.php
@@ -27,6 +27,7 @@ $sc->getRecentVisitors(500);
             <div class="row hoverable-color-fixed text-center">
             	<div class="col-lg-2 col-md-2 col-sm-2 text-center">
             		<h1><i class="fa fa-male fa-4x"></i><h1>
+            		<h1>' . ($i + 1) . '</h1>
             	</div>
             	<div class="col-lg-5 col-md-5 col-sm-5 text-center">
             		<table>
